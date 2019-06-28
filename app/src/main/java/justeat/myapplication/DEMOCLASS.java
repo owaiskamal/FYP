36f340dbbd7f8f1077a51d3f418c05d1d3f99683
PATHAN <owaiskamal12@gmail.com>
2019-06-28 18:52:53 +0500

    Initial commit

---
+++ app/src/main/java/justeat/myapplication/DEMOCLASS.java
@@ -0,0 +1,178 @@
+package justeat.myapplication;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import justeat.myapplication.R;
+
+import com.androidhiddencamera.CameraConfig;
+import com.androidhiddencamera.CameraError;
+import com.androidhiddencamera.HiddenCameraActivity;
+import com.androidhiddencamera.HiddenCameraUtils;
+import com.androidhiddencamera.config.CameraFacing;
+import com.androidhiddencamera.config.CameraImageFormat;
+import com.androidhiddencamera.config.CameraResolution;
+import com.androidhiddencamera.config.CameraRotation;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DEMOCLASS extends HiddenCameraActivity {
+    private static final int REQ_CODE_CAMERA_PERMISSION = 1253;
+
+    private CameraConfig mCameraConfig;
+    private static final String[] requiredPermissions = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA,
+    };
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dem_activity);
+
+        mCameraConfig = new CameraConfig()
+                .getBuilder(this)
+                .setCameraFacing(CameraFacing.FRONT_FACING_CAMERA)
+                .setCameraResolution(CameraResolution.HIGH_RESOLUTION)
+                .setImageFormat(CameraImageFormat.FORMAT_JPEG)
+                .setImageRotation(CameraRotation.ROTATION_270)
+                .build();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            //Start camera preview
+            startCamera(mCameraConfig);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    REQ_CODE_CAMERA_PERMISSION);
+        }
+     //Take a picture
+        findViewById(R.id.capture_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Take picture using the camera without preview.
+                takePicture();
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onImageCapture(@NonNull File imageFile) {
+
+        // Convert file to bitmap.
+        // Do something.
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+        Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath(), options);
+
+        FirebaseApp.initializeApp(DEMOCLASS.this);
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionFaceDetectorOptions option =
+                new FirebaseVisionFaceDetectorOptions.Builder()
+                        .setPerformanceMode(FirebaseVisionFaceDetectorOptions.FAST)
+                        .setLandmarkMode(FirebaseVisionFaceDetectorOptions.NO_LANDMARKS)
+                        .setClassificationMode(FirebaseVisionFaceDetectorOptions.NO_CLASSIFICATIONS)
+                        .build();
+        FirebaseVisionFaceDetector detector = FirebaseVision.getInstance().getVisionFaceDetector(option);
+        Task<List<FirebaseVisionFace>> result = detector
+                .detectInImage(image)
+                .addOnSuccessListener(new
+                                              OnSuccessListener<List<FirebaseVisionFace>>() {
+                                                  @Override
+                                                  public void onSuccess(List<FirebaseVisionFace> faces) {
+                                                      Log.d("OWA" , "DONE");
+
+                                                  }
+
+                                              })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("OWA" , "NOT DOENEE");
+                    }
+                });
+
+    }
+
+
+
+
+        //Display the image to the image view
+
+
+
+    @Override
+    public void onCameraError(@CameraError.CameraErrorCodes int errorCode) {
+        switch (errorCode) {
+            case CameraError.ERROR_CAMERA_OPEN_FAILED:
+                //Camera open failed. Probably because another application
+                //is using the camera
+                break;
+            case CameraError.ERROR_IMAGE_WRITE_FAILED:
+                //Image write failed. Please check if you have provided WRITE_EXTERNAL_STORAGE permission
+                break;
+            case CameraError.ERROR_CAMERA_PERMISSION_NOT_AVAILABLE:
+                //camera permission is not available
+                //Ask for the camra permission before initializing it.
+                break;
+            case CameraError.ERROR_DOES_NOT_HAVE_OVERDRAW_PERMISSION:
+                //Display information dialog to the user with steps to grant "Draw over other app"
+                //permission for the app.
+                HiddenCameraUtils.openDrawOverPermissionSetting(this);
+                break;
+            case CameraError.ERROR_DOES_NOT_HAVE_FRONT_CAMERA:
+                Toast.makeText(this, "Your device does not have front camera.", Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == REQ_CODE_CAMERA_PERMISSION) {
+
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startCamera(mCameraConfig);
+            } else {
+                Toast.makeText(this, "DENIEED", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
+}
---
+++ app/src/main/java/justeat/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package justeat.myapplication;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.androidhiddencamera.HiddenCameraFragment;
+import com.google.firebase.FirebaseApp;
+
+public class MainActivity extends AppCompatActivity {
+
+    private HiddenCameraFragment mHiddenCameraFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(MainActivity.this);
+        findViewById(R.id.btn_cick).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mHiddenCameraFragment != null) {    //Remove fragment from container if present
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .remove(mHiddenCameraFragment)
+                            .commit();
+                    mHiddenCameraFragment = null;
+                }
+
+                startActivity(new Intent(MainActivity.this, DEMOCLASS.class));
+            }
+        });
+    }
+
+
+}
