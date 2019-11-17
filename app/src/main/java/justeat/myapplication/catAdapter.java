a669af92fdb8ae0aef7b97c9cc2a15ba62be6b58
hamza <htariq301@gmail.com>
2019-11-17 22:19:46 +0500

    recycler no ui

--- app/src/main/java/justeat/myapplication/catAdapter.java
+++ app/src/main/java/justeat/myapplication/catAdapter.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -35,6 +37,8 @@ public class catAdapter extends RecyclerView.Adapter<catAdapter.myViewHolder> {
     public void onBindViewHolder(@NonNull myViewHolder holder, int position) {
        holder.name.setText(categories.get(position).getCategoryName());
         Picasso.get().load(categories.get(position).getUrl()).into(holder.picture);
+            holder.btn.setVisibility(View.VISIBLE);
+
     }
 
     @Override
@@ -46,10 +50,21 @@ public class catAdapter extends RecyclerView.Adapter<catAdapter.myViewHolder> {
     {
         TextView name;
             ImageView picture;
+            Button btn;
         public myViewHolder(View itemView) {
             super(itemView);
             name= (TextView) itemView.findViewById(R.id.name);
            picture=(ImageView )itemView.findViewById(R.id.category);
+           btn=(Button)itemView.findViewById(R.id.checkDetails);
+        }
+        public  void onClick(int position){
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(context,position+"is clicked",Toast.LENGTH_SHORT).show();
+                }
+            });
+
         }
     }
 }
