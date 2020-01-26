ddfedd151303c83b605572d0a85ac6aeaf42e88b
PATHAN <owaiskamal12@gmail.com>
2020-01-26 21:46:00 +0500

    table number

---
+++ app/src/main/java/justeat/myapplication/Model/orderList.java
@@ -0,0 +1,52 @@
+package justeat.myapplication.Model;
+
+import java.util.List;
+
+public class orderList {
+   private String name;
+    private String tableNo;
+    private String totalPrice;
+    private List<Cart> order ;
+
+    public orderList() {
+    }
+
+    public orderList(String name, String tableNo , String totalPrice , List<Cart> order) {
+        this.name = name;
+        this.tableNo = tableNo;
+        this.order = order;
+        this.totalPrice = totalPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(String totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTableNo() {
+        return tableNo;
+    }
+
+    public void setTableNo(String tableNo) {
+        this.tableNo = tableNo;
+    }
+
+    public List<Cart> getOrder() {
+        return order;
+    }
+
+    public void setOrder(List<Cart> order) {
+        this.order = order;
+    }
+}
